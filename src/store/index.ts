66db4c5b598b81b5812d13e2a899138f81c7742a
Your Name <you@example.com>
2020-04-20 11:24:23 -0600

    readme update

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,17 @@
 import {combineReducers, createStore, applyMiddleware} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { profileReducer } from './reducers/reducers';
+
+const rootReducer = combineReducers({
+    profile : profileReducer 
+})
+
+export default function configureStore(){
+    const store = createStore(
+        rootReducer,
+        composeWithDevTools(
+            applyMiddleware()
+        )
+    );
+    return store;
+}
