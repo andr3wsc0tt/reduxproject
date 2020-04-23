69c8b8d2730bb513de3c36b1ef71f392057e9efd
Mohammad <adawi83@hotmail.com>
2020-04-23 08:42:30 -0600

    Profile page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,66 +1,9 @@
 import * as React from 'react';
-import { Divider,Button, Form, Grid, Header, Segment, Container,Input} from 'semantic-ui-react'
-import {checkPass} from '../store/actions/actions'
-import {ProfileActionTypes, Profile} from '../store/types/types'
-import { RootState } from '../store';
-import { connect } from 'react-redux';
-import { BrowserRouter as Router, Link, Redirect } from "react-router-dom";
-import ProfilePage from './ProfilePage';
-
+import { Divider,Button, Form, Grid, Header, Segment, Container, Input,} from 'semantic-ui-react'
 export interface IHomeProps {
-  checkPass: typeof checkPass,
-  profiles: Profile[],
-  loggedIn: boolean,
-  location?: Router
 }
-
-export interface IHomeState{
-  userName: string,
-  passWord: string
-}
-
-export class Home extends React.Component<IHomeProps, IHomeState> {
-
-  constructor(props: IHomeProps){
-    super(props);
-    this.state = {userName: "", passWord: ""};
-  }
-
-  handleUserChange = (e : React.FormEvent<HTMLInputElement> ) => {
-    this.setState({userName : e.currentTarget.value})
-  }
-
-  handlePassChange = (e : React.FormEvent<HTMLInputElement> ) => {
-    this.setState({passWord : e.currentTarget.value})
-  }
-
-  handleOnClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent> ) => {
-    event.preventDefault();
-    let {userName, passWord} = this.state;
-    let {profiles, checkPass, loggedIn} = this.props;
-
-    let cred : Array<string> = [userName, passWord];
-    checkPass(cred);
-    this.setState({userName : "", passWord: ""});
-
-}
-
+export default class Home extends React.Component<IHomeProps> {
   public render() {
-    let {loggedIn, profiles } = this.props;
-    console.log(loggedIn);
-    if (loggedIn === true || sessionStorage.getItem('loggedIn') == 'true'){
-      const uName = profiles.filter(profile => profile.loggedIn == true);
-      sessionStorage.setItem('loggedIn', 'true');
-      sessionStorage.setItem('userName', uName[0].name);
-      return (
-        <>
-        <Router>
-        <Redirect to={`/profile/${uName[0].name}`}/>
-        <Link to="" component={ProfilePage}/>
-      </Router>
-      </>
-      )
-    }
     return (
       
       <Grid columns='equal'>
@@ -68,7 +11,7 @@ export class Home extends React.Component<IHomeProps, IHomeState> {
         <Grid.Column></Grid.Column>
         <Grid.Column></Grid.Column>
         <Grid.Column floated='right'> 
-            <Input fluid icon='user' iconPosition='left' placeholder='Username'  value={this.state.userName} onChange={this.handleUserChange} />
+            <Input fluid icon='user' iconPosition='left' placeholder='Username' />
             </Grid.Column >
             <Grid.Column floated='right'>
             <Input
@@ -77,12 +20,10 @@ export class Home extends React.Component<IHomeProps, IHomeState> {
               iconPosition='left'
               placeholder='Password'
               type='password'
-              value={this.state.passWord} 
-              onChange={this.handlePassChange}
             />
 </Grid.Column>
 <Grid.Column floated='right'>
-            <Button color='yellow' fluid size='large' onClick={this.handleOnClick}>
+            <Button color='yellow' fluid size='large'>
               Login
             </Button>
 </Grid.Column>
@@ -132,15 +73,3 @@ export class Home extends React.Component<IHomeProps, IHomeState> {
     );
   }
 }
-
-const mapStateToProps = (state: RootState, ownProps : IHomeProps) => {
-  return {
-    profiles: state.profile.profiles,
-    loggedIn: state.profile.loggedIn
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  {checkPass},
-)(Home);
