24519d40eb682e1786252709ddc517cf842c3ec9
CharlZ <charles.ceo2020@gmail.com>
2020-04-27 15:34:30 -0600

    mobile responsive features and functions added

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -49,6 +49,7 @@ export interface IHomeState {
   signupMessage: string
 }
 
+
 export class Home extends React.Component<IHomeProps, IHomeState> {
   constructor(props: IHomeProps) {
     super(props);
