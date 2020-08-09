5e323aea810085ff2fd09f3d6d98eb9befbd25a3
Your Name <you@example.com>
2020-08-09 14:19:57 -0600

    Updates

---
+++ precache-manifest.2ec12140b984b655b1f8540f9058be7f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0488c6940db95996ac6da5b4109459",
+    "url": "/reduxproject/index.html"
+  },
+  {
+    "revision": "64d3bccb2f729ccb78f7",
+    "url": "/reduxproject/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "396809c6d9f2e4855979",
+    "url": "/reduxproject/static/css/main.a1293761.chunk.css"
+  },
+  {
+    "revision": "64d3bccb2f729ccb78f7",
+    "url": "/reduxproject/static/js/2.57f67256.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/reduxproject/static/js/2.57f67256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "396809c6d9f2e4855979",
+    "url": "/reduxproject/static/js/main.d6d3c5fc.chunk.js"
+  },
+  {
+    "revision": "b23c9543d6d75c4c60ec",
+    "url": "/reduxproject/static/js/runtime-main.289344a0.js"
+  },
+  {
+    "revision": "45711a7fdb8fd68ebe9feb0b38fae4ba",
+    "url": "/reduxproject/static/media/Logo.45711a7f.png"
+  },
+  {
+    "revision": "f45e432ee9aadef3e60c28c7a062a5ac",
+    "url": "/reduxproject/static/media/profile.f45e432e.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/andr3wsc0tt/reduxproject/precache-manifest.bbe57cfae6764ddf5d82bda98da7948f.js"
+  "/reduxproject/precache-manifest.2ec12140b984b655b1f8540f9058be7f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/andr3wsc0tt/reduxproject/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reduxproject/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
